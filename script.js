5c0b3d943775dd3fb9cc1bbcd3fc624922fcd9d1
persephia <18404101+persephia@users.noreply.github.com>
2023-01-31 09:26:05 -0500

    Add HTML framework and computer choice function

---
+++ script.js
@@ -0,0 +1,23 @@
+// Definitions
+let userScore = 0;
+let computerScore = 0;
+let roundWinner = "";
+
+const computerChoice = getRandomChoice();
+
+// Computer Makes Choice
+function getRandomChoice() {
+    let randomChoiceNum = Math.floor(Math.random() * 3);
+    switch (randomChoiceNum) {
+        case 0:
+            return 'fire'
+        case 1:
+            return 'water'
+        case 2:
+            return 'grass'
+    }
+}
+//Testing ...
+// console.log(getRandomChoice());
+
+// User Makes Choice
