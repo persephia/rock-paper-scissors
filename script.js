2d609887eefa9768dd6a8e145efde27893d6abf7
persephia <18404101+persephia@users.noreply.github.com>
2023-02-14 09:45:43 -0500

    Linked js to html

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Definitions
 let userScore = 0;
 let computerScore = 0;
+let result = "";
 
-const computerChoice = getRandomChoice();
+const playerInput = document.querySelector("#playerText");
+const getPlayerChoiceButton = document.querySelector("#get-choice");
+
+if (getPlayerChoiceButton) {
+    getPlayerChoiceButton.addEventListener("click", fetchChoice);
+}
+
+function fetchChoice() {
+    let playerChoice = playerInput.value;
+    let userChoice = playerChoice.toLowerCase();
+    console.log(userChoice);
+}
+
 
 // Computer Makes Choice
 function getRandomChoice() {
@@ -16,7 +29,48 @@ function getRandomChoice() {
             return 'grass'
     }
 }
-//    //Testing ...
-//    // console.log(getRandomChoice());
+//    Testing ...
+//    console.log(computerChoice);
 
-// User Makes Choice
+// Play the Game
+var fireBtn = document.getElementById('fireBtn');
+if (fireBtn) {
+    fireBtn.addEventListener('click', () => getUserChoice());
+}
+
+var waterBtn = document.getElementById('waterBtn');
+if (waterBtn) {
+    waterBtn.addEventListener('click', () => getUserChoice('water'));
+}
+
+var grassBtn = document.getElementById('grassBtn');
+if (grassBtn) {
+    grassBtn.addEventListener('click', () => getUserChoice('grass'));
+}
+
+
+
+
+function getUserChoice() {
+   // const computerChoice = getRandomChoice();
+   // playGame(userChoice, computerChoice);
+   console.log("click");
+}
+
+function playGame(userChoice, computerChoice) {
+    if (userChoice == computerChoice) {
+        result = ("You both selected " + userChoice + ", so it's a tie.")
+    } else if ((userChoice == 'fire' && computerChoice == 'grass') || (userChoice == 'grass' && computerChoice == 'water' || (userChoice == 'water' && computerChoice == 'fire'))) {
+        userScore += 1;
+        result = ("Aha! " + userChoice + " defeats " + computerChoice + ".  Well done!")
+    } else {
+        computerScore += 1;
+        result = ("Alas, " + computerChoice + "defeats " + userChoice + ".  Better luck next time...")
+    }
+    document.getElementById('result').innerHTML = result;
+}
+
+
+
+
+
